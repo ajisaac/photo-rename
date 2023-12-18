366dca7599ba930c68fd7ed82069e09a1b23432f
Aaron Isaac <aaron@aisaac.co>
2023-12-17 21:20:51 -0800

    Add metadata functionality

--- src/main/java/co/aisaac/Main.java
+++ src/main/java/co/aisaac/Main.java
@@ -17,9 +17,9 @@ import java.util.Date;
 public class Main {
 
 	public static final DateTimeFormatter NEW_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd_HHmmss");
-	public static final String EXTENSION = ".png";
-//	public static final String EXTENSION = ".jpg";
-//	public static final String EXTENSION = ".jpeg";
+	//	public static final String EXTENSION = ".png";
+	public static final String EXTENSION = ".jpg";
+	//	public static final String EXTENSION = ".jpeg";
 	public static final boolean MOVE = false;
 
 	public static void main(String[] args) {
@@ -27,6 +27,79 @@ public class Main {
 	}
 
 	private void run() {
+//		imageCreateDateToName();
+		imageNameToDate();
+	}
+
+	private void imageNameToDate() {
+		File[] files = new File("/users/aaron/photos_backup/no_dates").listFiles();
+		if (files == null) return;
+
+		int i = 0;
+		for (File file : files) {
+
+			// validation
+			if (file.isDirectory()) continue;
+			if (!file.getName().endsWith(EXTENSION)) continue;
+			if (!file.getName().startsWith("IMG_")) continue;
+
+			if (i < 1000)
+				System.out.println(i + "\t\t" + file.getName());
+			else
+				System.out.println(i + "\t" + file.getName());
+
+			// find date created from filename
+			LocalDateTime dateCreatedFn = extractDateFromFilename(file.getName());
+			if (!verifyDateCreated(file, dateCreatedFn)) {
+				System.out.println("Date extracted from filename is not correct");
+				continue;
+			}
+			i++;
+
+			// find date created from metadata
+			LocalDateTime dateCreatedMd = findDateCreatedFromMetadata(file);
+			if (dateCreatedMd != null) {
+				System.out.println("Date metadata found");
+				boolean close = areClose(dateCreatedFn, dateCreatedMd);
+
+				if (!close) {
+					// A merge conflict of dates, handle manually
+					System.out.println("Dates are not close for " + file.getName());
+					break;
+				}
+
+				// has metadata, and is named appropriately
+				// move(file, dateCreatedMd);
+				continue;
+			}
+
+			// no metadata, but is named appropriately
+			// https://github.com/apache/commons-imaging/blob/master/src/test/java/org/apache/commons/imaging/examples/WriteExifMetadataExample.java
+			addDateCreatedMetadata(file, dateCreatedFn);
+			// move(file, dateCreated);
+
+		}
+
+	}
+
+	// add date created metadata to file
+	private void addDateCreatedMetadata(File file, LocalDateTime dateCreatedFn) {
+
+	}
+
+	/**
+	 * Verify that the date extraction function actually worked
+	 */
+	private boolean verifyDateCreated(File file, LocalDateTime dateCreatedFn) {
+		String name = file.getName();
+		String dateStr = "IMG_" + dateCreatedFn.format(NEW_DATE_FORMAT);
+		return name.startsWith(dateStr);
+	}
+
+	/**
+	 * Pull meta data about image creation date to rename the file
+	 */
+	private void imageCreateDateToName() {
 		// list all files
 		File[] files = new File("/users/aaron/photos_backup").listFiles();
 		if (files == null) return;
@@ -43,7 +116,7 @@ public class Main {
 				System.out.println(i + "\t" + file.getName());
 
 			// find date created from metadata
-			LocalDateTime dateCreated = findDateCreated(file);
+			LocalDateTime dateCreated = findDateCreatedFromMetadata(file);
 			if (dateCreated == null) continue;
 
 			i++;
@@ -71,7 +144,7 @@ public class Main {
 	}
 
 	// finds date created from metadata
-	private LocalDateTime findDateCreated(File file) {
+	private LocalDateTime findDateCreatedFromMetadata(File file) {
 		Metadata metadata;
 		try {
 			metadata = ImageMetadataReader.readMetadata(file);
@@ -99,13 +172,13 @@ public class Main {
 	}
 
 	// for extracting a date from a filename as needed
-	private LocalDateTime extractDate(String filename) {
-		String dateStr = filename.substring(4, 12);
+	private LocalDateTime extractDateFromFilename(String filename) {
+		String dateStr = filename.substring(4, 14);
 		String yearStr = dateStr.substring(0, 4);
-		String monthStr = dateStr.substring(4, 6);
-		String dayStr = dateStr.substring(6, 8);
+		String monthStr = dateStr.substring(5, 7);
+		String dayStr = dateStr.substring(8, 10);
 
-		String timeStr = filename.substring(13, 19);
+		String timeStr = filename.substring(15, 21);
 		String hourStr = timeStr.substring(0, 2);
 		String minuteStr = timeStr.substring(2, 4);
 		String secondStr = timeStr.substring(4, 6);
